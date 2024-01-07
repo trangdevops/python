48cbff498602f692647c97553bae0855a9f29640
trangdevops <trangdevops@gmail.com>
2024-01-07 22:19:13 +0700

    update Day2

---
+++ Day2_Beginner_Data_Types_and_Strings.py
@@ -0,0 +1,10 @@
+#Nếu hóa đơn là $150,00, chia cho 5 người, với 12% tiền boa.
+#Mỗi người phải trả (150,00/5) * 1,12 = 33,6
+#Định dạng kết quả thành 2 chữ số thập phân = 33,60
+#Do đó, phần chia của mọi người trong tổng hóa đơn là 30 USD cộng với tiền boa 3,60 USD.
+
+total_bill = input("Nhập số tiền hóa đơn: ")
+total_member = input("Nhập số người: ")
+tip_money = input("Số tiền khách tip: ")
+
+print(type(total_bill))
