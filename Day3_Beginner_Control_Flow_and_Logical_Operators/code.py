edc3cf3e1a9cecfede6f98f669374693543ac6e9
trangdevops <trangdevops@gmail.com>
2024-01-07 23:10:07 +0700

    update Day3

---
+++ Day3_Beginner_Control_Flow_and_Logical_Operators/code.py
@@ -0,0 +1,7 @@
+print("Chào đón bạn đến với công viên NamHaiZoo")
+height = int(input("Chiều cao của bạn bao nhiêu (cm): "))
+
+if height > 120:
+    print("\n Bạn vào công viên với giá 50.000vnđ")
+else:
+    print("\n Bạn được miễn phí vào công viên")
